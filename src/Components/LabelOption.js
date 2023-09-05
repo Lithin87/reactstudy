bf1c791821a7063a2df1019aa4725c94218fa200
Lamar Thomas <lithin.thomas@walgreens.com>
2023-09-05 17:14:16 +0530

    .

--- src/Components/LabelOption.js
+++ src/Components/LabelOption.js
@@ -16,7 +16,7 @@ function LabelOption({ children , htmlFor}) {
    console.log(url_r);
     let response = "";
     if ([3, 4, 5].includes(index)) 
-      response = await Axios.post(url_r, ctx.text).catch((error) => {console.log("Error accessing backend"+error);  ctx.setConsoleText(error);});
+     {console.log(ctx.text) ; response = await Axios.post(url_r, ctx.text).catch((error) => {console.log("Error accessing backend"+error);  ctx.setConsoleText(error);});}
   else
     response = await Axios.get(url_r).catch((error) => {console.log("Error accessing backend"+error);  ctx.setConsoleText(error);});
     if(response)
